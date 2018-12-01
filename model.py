83e8a0692893b765f79afea8c54f0e8e21d8bff8
Kamran Janjua <kamranejaz98@gmail.com>
2018-12-02 04:49:48 +0500

    Update model.py

--- model.py
+++ model.py
@@ -7,6 +7,9 @@ from keras import backend as K
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 import tflearn
+from keras import backend as K
+from keras.models import Model
+from keras.layers import Convolution2D, MaxPooling2D, Input, AtrousConvolution2D
 from tensorflow.contrib.layers import flatten
 import tensorflow.contrib.layers as initializers
 
@@ -35,6 +38,44 @@ def inference(input_images, reuse=False):
             #x = tflearn.prelu(feature)
             #x = slim.fully_connected(x, num_outputs=10, activation_fn=None, reuse=reuse, scope='fc2')
     return feature
+
+def dil_inf(x):
+    keep_prob = 0.5
+    conv1_filter = tf.Variable(tf.truncated_normal(shape=[3, 3, 1, 32], mean=0, stddev=0.08))
+    conv2_filter = tf.Variable(tf.truncated_normal(shape=[3, 3, 32, 128], mean=0, stddev=0.08))
+    conv3_filter = tf.Variable(tf.truncated_normal(shape=[5, 5, 128, 256], mean=0, stddev=0.08))
+    conv4_filter = tf.Variable(tf.truncated_normal(shape=[5, 5, 256, 512], mean=0, stddev=0.08))
+    conv1 = tf.nn.conv2d(x, conv1_filter, strides=[1,1,1,1], padding='SAME')
+    conv1 = tf.nn.relu(conv1)
+    conv1_pool = tf.nn.max_pool(conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    conv1_bn = tf.layers.batch_normalization(conv1_pool)
+    conv2 = tf.nn.conv2d(conv1_bn, conv2_filter, strides=[1,1,1,1], padding='SAME')
+    conv2 = tf.nn.relu(conv2)
+    conv2_pool = tf.nn.max_pool(conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')    
+    conv2_bn = tf.layers.batch_normalization(conv2_pool)
+    conv3 =  tf.nn.atrous_conv2d(conv2_bn, conv3_filter, 2, padding='SAME')
+    conv3 = tf.nn.relu(conv3)
+    conv3_pool = tf.nn.max_pool(conv3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')  
+    conv3_bn = tf.layers.batch_normalization(conv3_pool)
+    conv4 =  tf.nn.atrous_conv2d(conv3_bn, conv4_filter, 4, padding='SAME')
+    conv4 = tf.nn.relu(conv4)
+    conv4_pool = tf.nn.max_pool(conv4, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    conv4_bn = tf.layers.batch_normalization(conv4_pool)
+    flat = tf.contrib.layers.flatten(conv4_bn)  
+    full1 = tf.contrib.layers.fully_connected(inputs=flat, num_outputs=128, activation_fn=tf.nn.relu)
+    full1 = tf.nn.dropout(full1, keep_prob)
+    full1 = tf.layers.batch_normalization(full1)
+    full2 = tf.contrib.layers.fully_connected(inputs=full1, num_outputs=256, activation_fn=tf.nn.relu)
+    full2 = tf.nn.dropout(full2, keep_prob)
+    full2 = tf.layers.batch_normalization(full2)
+    full3 = tf.contrib.layers.fully_connected(inputs=full2, num_outputs=512, activation_fn=tf.nn.relu)
+    full3 = tf.nn.dropout(full3, keep_prob)
+    full3 = tf.layers.batch_normalization(full3)    
+    full4 = tf.contrib.layers.fully_connected(inputs=full3, num_outputs=1024, activation_fn=tf.nn.relu)
+    full4 = tf.nn.dropout(full4, keep_prob)
+    full4 = tf.layers.batch_normalization(full4)        
+    out = tf.contrib.layers.fully_connected(inputs=full3, num_outputs=10, activation_fn=None)
+    return out
 
 def lenet(x, reuse=False):
     mu = 0
@@ -177,9 +218,3 @@ def marginal_loss(model1, model2, y, margin, threshold):
 	ce_2 = ce_2*0.1
 
 	return tf.multiply(margin_, sum_) + tf.reduce_mean(ce_1+ce_2)/2
-
-
-
-
-
-
